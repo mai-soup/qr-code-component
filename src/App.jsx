782b825d90d638c3dab461dd31558cec8df505c2
Maijs Garais <maireadsbooks@gmail.com>
2023-03-15 07:52:22 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      Improve your front-end skills by building projects Scan the QR code to
+      visit Frontend Mentor and take your coding skills to the next level
+      <div class="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="https://mai-soup.github.io">Maijs Garais</a>.
+      </div>
+    </div>
+  );
+}
+
+export default App;
