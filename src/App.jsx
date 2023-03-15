a9202c231fc30bc3051916d194b1e20e80f2a60b
Maijs Garais <maireadsbooks@gmail.com>
2023-03-15 10:47:36 +0000

    add attribution in footer, minimal styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import "./App.css";
+import AttributionContainer from "./AttributionContainer";
+import QrContainer from "./QrContainer";
 
 function App() {
   return (
-    <div className="App">
-      Improve your front-end skills by building projects Scan the QR code to
-      visit Frontend Mentor and take your coding skills to the next level
-      <div class="attribution">
-        Challenge by{" "}
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-          Frontend Mentor
-        </a>
-        . Coded by <a href="https://mai-soup.github.io">Maijs Garais</a>.
-      </div>
+    <div className="flex flex-col h-full bg-light-grey">
+      <QrContainer />
+      <AttributionContainer />
     </div>
   );
 }
---
+++ src/QrContainer.jsx
@@ -0,0 +1,10 @@
+const QrContainer = () => {
+  return (
+    <div className="grow flex justify-center items-center">
+      Improve your front-end skills by building projects Scan the QR code to
+      visit Frontend Mentor and take your coding skills to the next level
+    </div>
+  );
+};
+
+export default QrContainer;
